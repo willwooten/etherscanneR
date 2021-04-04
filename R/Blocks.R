5928f5351a6b514861567af283f2655ddfd63066
willwooten <wwootencfc@gmail.com>
2021-04-04 14:13:29 -0500

    added estimated_block_countdown

--- R/Blocks.R
+++ R/Blocks.R
@@ -14,3 +14,24 @@ get_block_uncle_rewards <- function(block_num, api_key){
     uncle_inclusion_reward = df$uncleInclusionReward
   )
 }
+
+# Get Estimated Block Countdown Time by BlockNo
+
+estimated_block_countdown <- function(block_num, api_key){
+  url <- paste0("https://api.etherscan.io/api?module=block&action=getblockcountdown&blockno=", block_num, "&apikey=", api_key)
+  jsonlite::fromJSON(url)
+  
+  if(jsonlite::fromJSON(url)$status == "0"){
+    jsonlite::fromJSON(url)$result
+  } else {
+    df <- jsonlite::fromJSON(url)$result
+    data.frame(
+      current_block = df$CurrentBlock, 
+      countdown_block = df$CountdownBlock, 
+      remaining_blocks = df$RemainingBlock, 
+      est_time_seconds = df$EstimateTimeInSec
+    )
+  }
+}
+
+
