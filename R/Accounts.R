3b4baa0fa363b7fbe46bbb7e5370f12b55a042fe
willwooten <wwootencfc@gmail.com>
2021-04-04 02:13:37 -0500

    update

--- R/Accounts.R
+++ R/Accounts.R
@@ -32,23 +32,29 @@ get_internal_txs_by_txhash <- function(hash, api_key){
 # Get "Internal Transactions" by Block Range
 
 get_internal_txs_by_blockrange <- function(start_block = 0, end_block = 99999999, sort = "asc", page = 1){
-  url <- paste0("https://api.etherscan.io/api?module=account&action=txlistinternal&startblock=", start_block, "&endblock=", end_block, "&page=1&sort=asc&apikey=", api_key)
+  url <- paste0("https://api.etherscan.io/api?module=account&action=txlistinternal&startblock=", start_block, "&endblock=", end_block, "&page=", page, "&sort=asc&apikey=", api_key)
   jsonlite::fromJSON(url)$result
 }
 
 # Get a list of "ERC20 - Token Transfer Events" by Address
 # [Optional Parameters] startblock: starting blockNo to retrieve results, endblock: ending blockNo to retrieve results
 
-get_erc20_transfers_by_address <- function(address, api_key, start_block = 0, end_block = 99999999, sort = "asc"){
-  url <- paste0("https://api.etherscan.io/api?module=account&action=tokentx&address=", address, "&startblock=", start_block ,"&endblock=", end_block, "&sort=", sort, "&apikey=", api_key)
+get_erc20_transfers_by_address <- function(address, contract, api_key, start_block = 0, end_block = 99999999, sort = "asc"){
+  url <- paste0("https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=", contract ,"&address=", address, "&startblock=", start_block ,"&endblock=", end_block, "&sort=", sort, "&apikey=", api_key)
   jsonlite::fromJSON(url)$result
 }
 
-# Get a list of "ERC20 - Token Transfer Events" by Contract Address
+# Get a list of "ERC721 - Token Transfer Events" by Address
 
-get_erc20_transfers_by_contract <- function(contract_address, api_key, sort = "asc", page = 1){
-  url <- paste0("https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=", contract_address,"&page=", page, "&sort=", sort, "&apikey=", api_key)
+get_erc721_transfers_by_address <- function(address, contract, api_key, start_block = 0, end_block = 99999999, sort = "asc"){
+  url <- paste0("https://api.etherscan.io/api?module=account&action=tokennfttx&contractaddress=", contract ,"&address=", address, "&startblock=", start_block ,"&endblock=", end_block, "&sort=", sort, "&apikey=", api_key)
   jsonlite::fromJSON(url)$result
 }
 
+# Get list of Blocks Mined by Address
+# ** type = blocks (full blocks only) or uncles (uncle blocks only)
 
+get_blocks_mined_by_address <- function(address, api_key, block_type = "blocks",start_block = 0, end_block = 99999999, sort = "asc", page = 1){
+  url <- paste0("https://api.etherscan.io/api?module=account&action=getminedblocks&address=", address, "&blocktype=", block_type, "&startblock=", start_block ,"&endblock=", end_block, "&page=", page, "&sort=", sort, "&apikey=", api_key)
+  jsonlite::fromJSON(url)$result
+}
