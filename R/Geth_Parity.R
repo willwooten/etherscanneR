580ca566391624791edc8b6a75e72f1b58393ef7
willwooten <wwootencfc@gmail.com>
2021-04-04 18:48:46 -0500

    added eth_get_uncle_by_block_hex_index

--- R/Geth_Parity.R
+++ R/Geth_Parity.R
@@ -42,5 +42,33 @@ eth_get_block_by_number <- function(block_hex, api_key){
   df_list
 }
 
+# eth_getUncleByBlockNumberAndIndex
+# Returns information about a uncle by block number
 
+eth_get_uncle_by_block_hex_index <- function(block_hex, index = "0x0", api_key){
+  df_list <- list()
+  url <- paste0("https://api.etherscan.io/api?module=proxy&action=eth_getUncleByBlockNumberAndIndex&tag=", block_hex ,"&index=", index, "&apikey=", api_key)
+  df <- jsonlite::fromJSON(url)$result
+  df_list[[1]] <- data.frame(
+    difficulty = df$difficulty, 
+    extra_data = df$extraData, 
+    gas_limit = df$gasLimit, 
+    gas_used = df$gasUsed, 
+    block_hash = df$hash, 
+    logs_bloom = df$logsBloom, 
+    miner = df$miner, 
+    mix_hash = df$mixHash, 
+    nonce = df$nonce, 
+    block_hex = df$number, 
+    parent_hash = df$parentHash, 
+    receipts_root = df$receiptsRoot, 
+    sha3_uncles = df$sha3Uncles, 
+    size = df$size, 
+    state_root = df$stateRoot, 
+    timestamp = df$timestamp,
+    transaction_root = df$transactionsRoot
+  )
+  df_list[[2]] <- df$uncles
+  df_list
+}
 
